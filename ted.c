1a61ed916c520d69c8a53b0ca3bc840557c1ed25
Utsav Chatterjee <c.utsav1@gmail.com>
2019-11-09 17:43:33 +0530

    Fixed a few comments and turned off some flags.

--- ted.c
+++ ted.c
@@ -20,15 +20,19 @@ void enableRawMode() {
 	struct termios raw = orig_termios;
 	
 	/**
-	 *  Turn off ECHO. Causes typed keys to be printed in terminal. 
+	 *  Turn off ECHO. Causes every typed key to be printed in terminal. 
+	 *  Turn off ICANON, which disables reading line-by-line and reads input byte-by-byte
 	 *  Turn off Ctrl-C & Ctrl-Z signals (ISIG)
-	 *  Disable Ctrl-S and Ctrl-Q. Ctrl-S stops data transmission to terminal until you press Ctrl-Q.
-	 *
+	 *  Disable IXON flag - Ctrl-S and Ctrl-Q. Ctrl-S stops data transmission to terminal until you press Ctrl-Q.
+	 *  ISTRIP causes the 8th bit of each input byte to be stripped, meaning it will set it to 0.
 	 * */
 
 	// Input Flags
 	raw.c_iflag &= ~(
+			BRKINT	|
 			ICRNL	|
+			INPCK	|
+			ISTRIP	|
 			IXON   
 			);
 	
@@ -36,8 +40,12 @@ void enableRawMode() {
 	raw.c_oflag &= ~(
 			OPOST
 			);
+	
+	// C Flags
+	// CS8 sets the character size(CS) to 8 bits per byte. Usually set in modern terminals.
+	raw.c_cflag |= (CS8);
 
-	// Local Flags
+	// Local Flags - a dumping ground for other state
 	raw.c_lflag &= ~(
 			ECHO	|
 			ICANON  |
